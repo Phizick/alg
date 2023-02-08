86926924d41e1d5a1c6dea7858aa8550f8b5c6d9
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2023-02-08 10:41:29 +0300

    added markup string page

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { FC } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
-export const FibonacciPage: React.FC = () => {
+export const FibonacciPage: FC = () => {
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
      
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import stylesStringPage from './string.module.css'
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+        <div className={`${stylesStringPage.container}`}>
+      <Input
+          placeholder={'Введите текст'}
+          extraClass={'input-style'}
+          isLimitText={true}
+          maxLength={11}/>
+      <Button text={'Развернуть'}
+              extraClass={'button-style'}/>
+        </div>
     </SolutionLayout>
   );
 };
