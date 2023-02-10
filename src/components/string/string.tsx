ebcbaff7b8f53ad7f4497510bafdde53e79541e0
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2023-02-10 14:17:55 +0300

    useEffect doesn't work ¯\_(ツ)_/¯

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,14 +1,16 @@
-import React, {useEffect, useRef} from "react";
+import React, {MouseEvent, useEffect, useRef, useState} from "react";
 import {SolutionLayout} from "../ui/solution-layout/solution-layout";
 import {Input} from "../ui/input/input";
 import {Button} from "../ui/button/button";
 import stylesStringPage from './string.module.css'
 import {useForm} from "../../Utils/Hooks/useForm";
 import {ElementStates} from "../../types/element-states";
+import {DELAY_IN_MS} from "../../constants/delays";
+import {Circle} from "../ui/circle/circle";
 
 
 export const StringComponent: React.FC = () => {
-    const { values, handleChange, setValues } = useForm({inputLetters: ''});
+    const { values, handleChange, setValues } = useForm({inputLetters: null});
     const timeRef = useRef<ReturnType<typeof setTimeout> | null>(null);
     const moveRef = useRef<boolean>(false);
 
@@ -18,8 +20,17 @@ export const StringComponent: React.FC = () => {
         if (!firstElement) {
             defaultArr[firstElement] = { ...defaultArr[firstElement], type: ElementStates.Changing};
             defaultArr[lastElement] = { ...defaultArr[lastElement], type: ElementStates.Changing};
+        } else {
+            defaultArr[firstElement] = { ...defaultArr[firstElement], type: ElementStates.Changing};
+            defaultArr[lastElement] = { ...defaultArr[lastElement], type: ElementStates.Changing};
+            let value = defaultArr[firstElement - 1].element;
+            defaultArr[firstElement - 1] = { element: defaultArr[lastElement +1].element, type: ElementStates.Modified};
+            defaultArr[lastElement + 1] = {  element: value, type: ElementStates.Modified};
         }
+        return defaultArr
     }
+
+    const [ reversedArr, setReverseArr ] = useState(arrToString)
 
 
 
@@ -29,7 +40,43 @@ export const StringComponent: React.FC = () => {
         }
     }, []);
 
-    const listReverse = ()
+    const listReverse = (arr: typeof arrToString, firstItem: number, nextItem: number) => {
+        arr = arrayElementsReverse(arr, firstItem, nextItem)
+        setReverseArr(() => [...arr])
+
+        nextItem = nextItem - 1;
+        firstItem = firstItem +1;
+
+        if (nextItem + 1 >= firstItem) {
+            timeRef.current = setTimeout(() => {
+                listReverse(arr, firstItem, nextItem)
+            }, DELAY_IN_MS)
+        } else {
+            timeRef.current = setTimeout(() => {
+                arr = arr.map((item) => {
+                    return { ...item, type: ElementStates.Modified}
+                })
+                setReverseArr(() => [...arr])
+                moveRef.current = false
+            }, DELAY_IN_MS)
+        }
+    };
+
+    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+        moveRef.current = true;
+        let lettersArr = Array.from(values.inputLetters as string)
+        let start = 0;
+        let end = lettersArr.length - 1
+        arrToString = lettersArr.map((item) => {
+            return { element: item, type: ElementStates.Default}
+        })
+        setReverseArr(() => [...arrToString])
+        timeRef.current = setTimeout(() => {
+            listReverse([...arrToString], start, end)
+        }, DELAY_IN_MS)
+    }
+
+    console.log(values.inputLetters)
 
 
 
@@ -40,9 +87,19 @@ export const StringComponent: React.FC = () => {
           placeholder={'Введите текст'}
           extraClass={'input-style'}
           isLimitText={true}
-          maxLength={11}/>
+          maxLength={11}
+      onChange={handleChange}/>
       <Button text={'Развернуть'}
-              extraClass={'button-style'}/>
+              extraClass={'button-style'}
+      onClick={handleClick}/>
+            <div className={`${stylesStringPage.latters}`}>
+                {!!reversedArr.length
+                ? reversedArr.map((item, index) => {
+                    return <Circle letter={item.element} key={index} state={item.type} data-testid={'circle'}/>
+                    })
+                : null}
+            </div>
+
         </div>
     </SolutionLayout>
   );
