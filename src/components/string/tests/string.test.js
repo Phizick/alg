ff071a98d154ac0421342eb4b6a38afd8a1d3f6c
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2023-03-08 16:18:31 +0300

    (circle/btn tests-file rebase) + string test

---
+++ src/components/string/tests/string.test.js
@@ -0,0 +1,20 @@
+import {reverseStringForTest} from '../../../Utils/String'
+
+describe('String component testing', () => {
+
+    it('reversing a string with an even number of items', () => {
+        expect(reverseStringForTest('012345')).toEqual(['5','4','3','2','1','0'])
+    })
+
+    it('reversing a string with an odd number of items', () => {
+        expect(reverseStringForTest('01234')).toEqual(['4','3','2','1','0'])
+    })
+
+    it('reversing a string with an one item', () => {
+        expect(reverseStringForTest('0')).toEqual(['0'])
+    })
+
+    it('empty line reversal', () => {
+        expect(reverseStringForTest('')).toEqual([])
+    })
+})
