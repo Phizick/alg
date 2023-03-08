3c306e008c6bbcee7749e98458352f24bbdab1b2
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2023-03-08 16:51:16 +0300

    sorting test pass

---
+++ src/constants/arraysForTests.ts
@@ -0,0 +1,46 @@
+import {ElementStates} from "../types/element-states";
+
+export const initialTestArr = [
+    {
+        item: 5,
+        state: ElementStates.Default
+    },
+    {
+        item: 8,
+        state: ElementStates.Default
+    },
+    {
+        item: 0,
+        state: ElementStates.Default
+    },
+    {
+        item: 2,
+        state: ElementStates.Default
+    },
+    {
+        item: 3,
+        state: ElementStates.Default
+    },
+]
+export const resultedTestArr = [
+    {
+        item: 8,
+        state: ElementStates.Default
+    },
+    {
+        item: 5,
+        state: ElementStates.Default
+    },
+    {
+        item: 3,
+        state: ElementStates.Default
+    },
+    {
+        item: 2,
+        state: ElementStates.Default
+    },
+    {
+        item: 0,
+        state: ElementStates.Default
+    },
+]
