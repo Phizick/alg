6ccd24d33de5b7e3b816bed28ea2f8dc489c5458
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2023-03-09 14:13:38 +0300

    queue test removed test

--- cypress/e2e/test/queue.cy.js
+++ cypress/e2e/test/queue.cy.js
@@ -94,6 +94,46 @@ describe('testing the correct operation of the queue component', () => {
 
     it('check for remove items to the queue', () => {
 
+        cy.get(cyInput).type('0')
+        cy.get(cySubmitBtn).click()
+        cy.wait(SHORT_DELAY_IN_MS)
+        cy.get(cyInput).type('1')
+        cy.get(cySubmitBtn).click()
+        cy.wait(SHORT_DELAY_IN_MS)
+        cy.get(cyInput).type('2')
+        cy.get(cySubmitBtn).click()
+
+        cy.get(cyCircleHead)
+            .first()
+            .should($div => {
+                expect($div).to.have.text('head')
+            })
+        cy.get(cyCircleTail)
+            .eq(2)
+            .should($div => {
+                expect($div).to.have.text('tail')
+            })
+
+        cy.wait(SHORT_DELAY_IN_MS)
+
+        cy.get(cyRemoveBtn).click()
+
+        cy.get(circles)
+            .first()
+            .invoke('attr', 'class')
+            .then(classList => expect(classList).contains('circle_changing'))
+
+        cy.wait(SHORT_DELAY_IN_MS)
+
+        cy.get(circles)
+            .first()
+            .should($div => {
+                expect($div).to.have.text('')
+            })
+            .invoke('attr', 'class')
+            .then(classList => expect(classList).contains('circle_default'))
+
+
 
     })
 
